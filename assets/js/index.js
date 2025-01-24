356854bcb8463467cb1c73b69d0c02bfb5f84224
Luca <zuin.luca1111@gmail.comgit config --global user.email zuin.luca1111@gmail.com>
2025-01-24 12:44:47 +0100

    commit 2

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+fetch("https://striveschool-api.herokuapp.com/api/product/", {
+  headers: {
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzkzNjI5Y2I3NDcwMTAwMTU4YjJiMzciLCJpYXQiOjE3Mzc3MTIyODQsImV4cCI6MTczODkyMTg4NH0.Qa4h_QJ6leyscheHpP8miF8JiP73g84x3k4PWuwZvnE",
+  },
+});
